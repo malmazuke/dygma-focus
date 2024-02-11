8ded6c0ab65397d6c995fb95b95d149c53fa2df7
Mark Feaver <mark.feaver@gmail.com>
2024-02-12 09:56:30 +1100

    Delete some template code

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -5,10 +5,5 @@ describe('Focus API Initialization', () => {
     const focus = new Focus();
     expect(focus).toBeDefined();
     expect(focus).toBeInstanceOf(Focus);
-
-    // If you have default properties set in your constructor, you can also check for those
-    expect(focus.closed).toBe(true);
-    expect(focus.debug).toBe(false);
-    // Add any other initial state checks here
   });
 });
